bc4c3d90644a51189341e611d9d638ce71215b15
NovaViper <nova.gamez15+code@gmail.com>
2017-05-28 17:08:10 -0500

    Fixed In-Game Config again

--- src/main/java/com/sixonethree/durabilityshow/client/gui/GuiFactory.java
+++ src/main/java/com/sixonethree/durabilityshow/client/gui/GuiFactory.java
@@ -1,31 +1,45 @@
 package com.sixonethree.durabilityshow.client.gui;
 
+import javax.annotation.Nullable;
 import java.util.Set;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.client.IModGuiFactory;
 
+/**
+ * Created by NovaViper on 4/4/2017.
+ */
 public class GuiFactory implements IModGuiFactory {
-	@Override public void initialize(Minecraft minecraftInstance) {}
+	@Override
+	public void initialize(Minecraft minecraftInstance) {
 	
-	@Override public Class<? extends GuiScreen> mainConfigGuiClass() {
+	}
+	
+	@Override
+	public boolean hasConfigGui() {
+		return true;
+	}
+	
+	@Override
+	public GuiScreen createConfigGui(GuiScreen parentScreen) {
+		return new ModGuiConfig(parentScreen);
+	}
+	
+	@Override
+	public Class<? extends GuiScreen> mainConfigGuiClass() {
 		return ModGuiConfig.class;
 	}
 	
-	@Override public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
+	@Nullable
+	@Override
+	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
 		return null;
 	}
-
-	@Override public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
-		return null;
-	}
-
-	@Override public boolean hasConfigGui() {
-		return false;
-	}
-
-	@Override public GuiScreen createConfigGui(GuiScreen parentScreen) {
+	
+	@Nullable
+	@Override
+	public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
 		return null;
 	}
 }
